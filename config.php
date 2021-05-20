a374d707328239f542939bcdc3403d68544bfcf4
Rob Watson <rob.watson@me.com>
2021-05-20 15:38:45 +0100

    Chagne redis host name

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 const LISTEN_PORT = 10005;
 const CLIENT_PASSWORD = 'password';
 
-const REDIS_HOST = 'eventqueue';
+const REDIS_HOST = 'queue';
 const REDIS_PORT = 6379;
 
 const REDIS_QUEUE_PREFIX = 'event:';
